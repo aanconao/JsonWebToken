9305c164e5fd9c0720d6e6d8689007df7bd415ca
aanconao <alan_ancona_ortiz@hotmail.com>
2024-05-10 16:22:34 +0200

    nose

---
+++ components/Login.tsx
@@ -0,0 +1,12 @@
+const Login = () => (
+  <div>
+    <form method="POST">
+      <input name="usr" placeholder="user" type="test" />
+      <br />
+      <input name="pwd" placeholder="password" type="test" />
+      <br />
+      <button type="submmit" />
+    </form>
+  </div>
+);
+export default Login;
---
+++ routes/login.tsx
@@ -0,0 +1,5 @@
+import Login from "../components/Login.tsx";
+
+export const Page = () => <Login />;
+
+export default Page;
